a8130a9f4ab95c96682488c68c7127337e0dd330
Ayush <raiayush402@gmail.com>
2023-07-10 11:38:38 +0530

    added request model

--- elevator/models.py
+++ elevator/models.py
@@ -23,9 +23,19 @@ class Elevator(models.Model):
     elevator_status = models.CharField(choices=elevator_status_choices, default='Halt')
 
 
-# class Request(models.Model):
-#     """
-#     Model for a Request
-#     """
+class Request(models.Model):
+    """
+    Model for a Request
+    """
+    STATUS_CHOICES = (
+        ('Active', 'Active'),
+        ('Fulfilled', 'Fulfilled')
+    )
+    pick_up_floor = models.BigIntegerField()
+    destination_floor = models.BigIntegerField()
+    elevator = models.ForeignKey(Elevator, on_delete=models.CASCADE) 
+    status = models.CharField(choices=STATUS_CHOICES, default='Active')
 
+    def __str__(self):
+        return 'Request for' + self.elevator
 
